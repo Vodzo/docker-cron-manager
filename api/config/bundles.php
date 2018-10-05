f6f7e3f0f2545e2b763ac8b1cf09b24d58e26243
Silvester Kokai <kokais93@gmail.com>
2018-10-05 15:29:34 +0200

    Added nelmio/api-doc-bundle

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
