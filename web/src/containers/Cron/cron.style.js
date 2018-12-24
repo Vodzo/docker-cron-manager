8c7269f9dbc0d044d31f672a05b5b92963643d5f
Vojislav Zelic <zelicv@gmail.com>
2018-12-25 00:16:15 +0100

    responsive tweaks

--- web/src/containers/Cron/cron.style.js
+++ web/src/containers/Cron/cron.style.js
@@ -1,7 +1,10 @@
-const styles = () => ({
+const styles = theme => ({
   paper: {
     width: '100%',
-    padding: '20px',
+    padding: theme.spacing.unit * 3,
+    '@media only screen and (max-width: 600px)': {
+      padding: theme.spacing.unit,
+    },
   },
 });
 
